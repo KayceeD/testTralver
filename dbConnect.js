1d8e829240085e4e8270dcd99bf6636c7f6a1c35
Tng_Dng <t.dng08@gmail.com>
2023-06-23 01:24:45 +0700

    del ,

--- dbConnect.js
+++ dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const dbConnect = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("connected");
     } catch (error) {
         console.log(error);

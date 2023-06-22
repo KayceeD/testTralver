f6d875a68c8156b06426312bc951048ad7283508
Tng_Dng <t.dng08@gmail.com>
2023-06-23 01:32:57 +0700

    sss

--- dbConnect.js
+++ dbConnect.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
 const dbConnect = async() => {
+    const mongo_url = process.env.MONGO_URL
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(mongo_url);
         console.log("connected");
     } catch (error) {
         console.log(error);
